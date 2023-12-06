f82d481225d790385846c74e338d22fa56d67e5e
Branislav Jenco <branislav.jenco@sesam.io>
2023-12-06 19:28:00 +0100

    Naive but working solution for day6

--- day6/solution.py
+++ day6/solution.py
@@ -21,13 +21,12 @@ class Race:
     record: int
 
     def better_strategies(self):
-        strategies = []
+        num = 0
         for i in range(self.time):
             s = Strategy(i, self.time)
             if s.distance > self.record:
-                strategies.append(s)
-        return strategies
-
+                num = num + 1
+        return num
 
 
 def parse(_inp):
@@ -36,13 +35,13 @@ def parse(_inp):
     races = []
     for i in range(len(times)):
         races.append(Race(times[i], records[i]))
-    return races    
+    return races
 
 def part1(_inp):
     races = parse(_inp)
     result = 1
     for r in races:
-        result = result * len(r.better_strategies())
+        result = result * r.better_strategies()
     return result
 
 test_input = file_into_list("day6/test_input1.txt")
@@ -52,14 +51,21 @@ real_input = file_into_list("day6/input.txt")
 print(part1(real_input))
 
 
+def parse2(_inp):
+    times = _inp[0].split(":")[1].split()
+    records = _inp[1].split(":")[1].split()
+    time = "".join(times)
+    record = "".join(records)
+    race = Race(int(time), int(record))
+    return race
 
 def part2(_inp):
-    pass
+    race = parse2(_inp)
+    return race.better_strategies()
 
 
-#test_input = file_into_list("day6/test_input2.txt")
-#expected = int(file_into_string("day6/test_output2.txt"))
-#test(part2, [test_input], [expected])
-#
-#real_input = file_into_list("day6/input.txt")
-#print(part2(real_input))
+test_input = file_into_list("day6/test_input2.txt")
+expected = int(file_into_string("day6/test_output2.txt"))
+test(part2, [test_input], [expected])
+real_input = file_into_list("day6/input.txt")
+print(part2(real_input))
